ebfd4d7c2b0bee2eac17b94fc0ca3be5b03efd1f
PauMB <paumb@MacBook-Pro-de-PauMB.local>
2021-01-24 13:09:00 +0100

    publicar pages

---
+++ 06-twittor/js/sw-utils.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @param {*} dynamicCache 
+ * @param {*} req 
+ * @param {*} res 
+ */
+function actualizarCacheDinamico( dynamicCache, req, res) {
+
+    if(res.ok) {
+        return caches.open(dynamicCache).then(cache => {
+            cache.put(req, res.clone());
+            return res.clone();
+        });
+    } else {
+        return res;
+    }
+
+
+}
---
+++ 06-twittor/sw.js
@@ -0,0 +1,78 @@
+importScripts('js/sw-utils.js');
+
+const STATIC_CACHE = 'static-v1';
+const DYNAMIC_CACHE = 'dynamic-v1';
+const INMUTABLE_CHACHE = 'inmutable-v1';
+
+const APP_SHELL = [
+    '/',
+    'index.html',
+    'css/style.css',
+    'img/favicon.ico',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
+    'js/app.js',
+    'js/sw-utils.js'
+];
+
+const APP_SHELL_INMUTABLE = [
+    'https://fonts.googleapis.com/css?family=Quicksand:300,400',
+    'https://fonts.googleapis.com/css?family=Lato:400,300',
+    'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
+    'css/style.css',
+    'js/libs/jquery.js'
+];
+
+self.addEventListener('install', e => {
+
+    //almacenamos en cache
+    const cacheStatic = caches.open( STATIC_CACHE ).then( cache => 
+        cache.addAll(APP_SHELL)
+    ); 
+
+    const cacheInmutable = caches.open( INMUTABLE_CHACHE ).then( cache => 
+        cache.addAll(APP_SHELL_INMUTABLE)
+    );
+
+    e.waitUntil(Promise.all([cacheStatic, cacheInmutable]))
+
+});
+
+self.addEventListener('activate', event => {
+
+    //Eliminamos caches antiguas
+    const keysCache = caches.keys().then( keys => {
+        keys.forEach( key => {
+            if(key !== DYNAMIC_CACHE && keys.includes('static')){
+                return caches.delete(key);
+            }
+        });
+    });
+
+    event.waitUntil(keysCache);
+});
+
+//Polita de caches
+self.addEventListener('fetch', event => {
+    
+    const respuesta = caches.match( event.request ).then(res => {
+
+        if(res) {
+            return res;
+        } else {
+            return fetch( event.request ).then( newResp => {
+
+                return actualizarCacheDinamico(DYNAMIC_CACHE, event.request, newResp);
+
+            });
+        }
+
+
+    });
+
+    event.respondWith( respuesta );
+
+});
